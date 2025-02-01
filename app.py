fa7eef2fd69bbdcb049170bb44c94668f857188e
AL Musheer <musheerayan@gmail.com>
2025-02-01 19:44:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import os
 from flask import Flask, render_template, request, send_file
 from rembg import remove
 from PIL import Image
 import io
-import os  # Ensure you import os to access environment variables
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -28,5 +28,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Modify this line to use the dynamic port
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
